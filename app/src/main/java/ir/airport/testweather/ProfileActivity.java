094c51f2cafab0b9b5550328f7784620883dab55
Alisalehivalo <55212861+Alisalehivalo@users.noreply.github.com>
2019-12-09 02:55:59 -0800

    Version 1.6 user profile added

--- app/src/main/java/ir/airport/testweather/ProfileActivity.java
+++ app/src/main/java/ir/airport/testweather/ProfileActivity.java
@@ -19,10 +19,157 @@ import com.google.firebase.auth.FirebaseUser;
 public class ProfileActivity extends AppCompatActivity {
 
 
+    private FirebaseAuth mAuth;
+    private FirebaseAuth.AuthStateListener authListener;
+
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profile);
+        Button btnChangePassword = findViewById(R.id.change_password_button);
+        Button btnChangeEmail = findViewById(R.id.change_email_button);
+        Button btnSendResetEmail = findViewById(R.id.sending_pass_reset_button);
+        Button btnRemoveUser = findViewById(R.id.remove_user_button);
+        Button signOut = findViewById(R.id.sign_out);
+
+        final EditText oldEmail = findViewById(R.id.old_email);
+        final EditText newEmail = findViewById(R.id.new_email);
+        final EditText newPassword = findViewById(R.id.newPassword);
+        EditText oldPassword = findViewById(R.id.old_password);
+        mAuth = FirebaseAuth.getInstance();
+        final FirebaseUser User = mAuth.getCurrentUser();
+        authListener = new FirebaseAuth.AuthStateListener() {
+            @Override
+            public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
+                FirebaseUser user = firebaseAuth.getCurrentUser();
+                if (user == null) {
+                    // user auth state is changed - user is null
+                    // launch login activity
+                    startActivity(new Intent(ProfileActivity.this, RegisterActivity.class));
+                    finish();
+                }
+            }
+        };
+        btnChangeEmail.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                if (User != null && !newEmail.getText().toString().trim().equals("")) {
+                    User.updateEmail(newEmail.getText().toString().trim())
+                            .addOnCompleteListener(new OnCompleteListener<Void>() {
+                                @Override
+                                public void onComplete(@NonNull Task<Void> task) {
+                                    if (task.isSuccessful()) {
+                                        Toast.makeText(ProfileActivity.this, "Email address is updated!", Toast.LENGTH_LONG).show();
+
+                                    } else {
+                                        Toast.makeText(ProfileActivity.this, "Failed to update email!", Toast.LENGTH_LONG).show();
+
+                                    }
+                                }
+                            });
+                } else if (newEmail.getText().toString().trim().equals("")) {
+                    newEmail.setError("Enter email");
+
+                }
+            }
+        });
+        btnChangePassword.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                if (User != null && !newPassword.getText().toString().trim().equals("")) {
+                    if (newPassword.getText().toString().trim().length() < 6) {
+                        newPassword.setError("Password too short, enter minimum 6 characters");
+
+                    } else {
+                        User.updatePassword(newPassword.getText().toString().trim())
+                                .addOnCompleteListener(new OnCompleteListener<Void>() {
+                                    @Override
+                                    public void onComplete(@NonNull Task<Void> task) {
+                                        if (task.isSuccessful()) {
+                                            Toast.makeText(ProfileActivity.this, "Password is updated, sign in with new password!", Toast.LENGTH_SHORT).show();
+
+                                        } else {
+                                            Toast.makeText(ProfileActivity.this, "Failed to update password!", Toast.LENGTH_SHORT).show();
+
+                                        }
+                                    }
+                                });
+                    }
+                } else if (newPassword.getText().toString().trim().equals("")) {
+                    newPassword.setError("Enter password");
+
+                }
+            }
+        });
+        btnSendResetEmail.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                if (!oldEmail.getText().toString().trim().equals("")) {
+                    mAuth.sendPasswordResetEmail(oldEmail.getText().toString().trim())
+                            .addOnCompleteListener(new OnCompleteListener<Void>() {
+                                @Override
+                                public void onComplete(@NonNull Task<Void> task) {
+                                    if (task.isSuccessful()) {
+                                        Toast.makeText(ProfileActivity.this, "Reset password email is sent!", Toast.LENGTH_SHORT).show();
+
+                                    } else {
+                                        Toast.makeText(ProfileActivity.this, "Failed to send reset email!", Toast.LENGTH_SHORT).show();
+
+                                    }
+                                }
+                            });
+                } else {
+                    oldEmail.setError("Enter email");
+
+                }
+            }
+        });
+
+        btnRemoveUser.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                if (User != null) {
+                    User.delete()
+                            .addOnCompleteListener(new OnCompleteListener<Void>() {
+                                @Override
+                                public void onComplete(@NonNull Task<Void> task) {
+                                    if (task.isSuccessful()) {
+                                        Toast.makeText(ProfileActivity.this, "Your profile is deleted:( Create a account now!", Toast.LENGTH_SHORT).show();
+                                        startActivity(new Intent(ProfileActivity.this, RegisterActivity.class));
+                                        finish();
+
+                                    } else {
+                                        Toast.makeText(ProfileActivity.this, "Failed to delete your account!", Toast.LENGTH_SHORT).show();
+
+                                    }
+                                }
+                            });
+                }
+            }
+        });
+
+        signOut.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mAuth.signOut();
+            }
+        });
+    }
+        
 
     }
-}
+
+    //sign out method
+
+
+
+
+
+
+
